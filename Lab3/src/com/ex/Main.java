19b74ded5ba17018490392be5bd1af5e33669b31
916-Pop-Felix <felcris2001@gmail.com>
2022-11-07 14:04:49 +0200

    Added lab3 documentation

--- Lab3/src/com/ex/Main.java
+++ Lab3/src/com/ex/Main.java
@@ -1,18 +1,15 @@
 package com.ex;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Map;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         LanguageScanner scanner = new LanguageScanner("programs/p1err.txt","res/tokens.in");
         try{
             scanner.scanProgram();
+            System.out.println("Lexically correct!");
         }
         catch (ScannerException se){
             System.out.println(se.getMessage());
